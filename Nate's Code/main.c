8dfcb37446fee8a91d46df50ed60911d6bbe6f76
BHASVIC-NateWhelan25 <nate.whelan25@students.bhasvic.ac.uk>
2025-11-06 15:44:36 +0000

    Added a few lists

--- Nate's Code/main.c
+++ Nate's Code/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int availableRooms[6];
+char mainGuestNames[6][2][15];
+char bookingIDs[6][20];
+int numAdults[6];
+int numChilds[6];
+
 int main(void) {
-    printf("Hello, World!\n");
+
+
+
     return 0;
 }
