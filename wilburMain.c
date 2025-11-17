081ced171aeb3d5dfc56cebf4b0ca82c21a7a2b5
BHASVIC-Wilbur-D <wilbur.duncan25@students.bhasvic.ac.uk>
2025-11-17 09:41:32 +0000

    Update wilburMain.c

--- wilburMain.c
+++ wilburMain.c
@@ -8,17 +8,20 @@ int i =0;
 int roomRate[6] = {100,100,85,75,75,50};
 int nights[6]= {1, 3, 2, 5, 4, 0};
 int mainGuestAge[6] = {36, 27, 0, 0, 0, 0};
-double boardRate[6] = {20.0, 10.0 ,5.0, 0.0, 0.0, 5.0};
+int boardRate[6] = {20, 10 ,5, 0, 0, 5};
 int Newspaper[6] = {1, 0, 0, 1, 1, 0};
 int guestAges[3][6] = {12, 18, 26};
 int guestCount[6] = {2, 0, 1, 3, 0, 0};
-
-
+char bookingID[6] = {"WD2161","","","","",""};
+char mainGuestFirstName[6] = {"Nate","","","","",""};
+double total[6] = {0, 0, 0, 0, 0, 0};
+char mainGuestSecondName = {"Whelan","","","","",""};
+double roomTotal[6] = {0.0,0.0,0.0,0.0,0.0,0.0};
 int main() {
     double roomTotal = calcRoomTotal(roomRate, nights[6], mainGuestAge[6]);
     double boardTotal = calcBoardTotal(boardRate[6], nights[6], guestAges[3][6], guestCount[6]);
     double newspaperCost = calcNewspaper(Newspaper[6]);
-    billCost(bookingID[6], mainGuestFirstName[6],mainGuestSecondName[6], roomTotal, boardTotal, newspaperCost);
+    billCost(bookingID[6], mainGuestFirstName[6],mainGuestSecondName[6], roomTotal[6], boardTotal[6], newspaperCost[6]);
     return 0;
 }
 
@@ -31,8 +34,8 @@ double calcRoomTotal(roomRate[4][6], nights[6], mainGuestAge[6]) {
 }
 
 
-double calcBoardTotal(double boardRate[4][6], double nights[6], int guestAges[3][6], int guestCount[6]) {
-    double total[i] = 0.0;
+double calcBoardTotal(int boardRate[4][6], int nights[6], int guestAges[3][6], int guestCount[6]) {
+    
 
     for (int n = 0; n <= guestCount[i]; n++) {
         double cost[n] = boardRate[n][6] * nights;
@@ -43,7 +46,7 @@ double calcBoardTotal(double boardRate[4][6], double nights[6], int guestAges[3]
         total[i] = total[i] + cost[i];
     }
 
-    return total;
+    return total[i];
 }
 double calcNewspaper(int Newspaper[6]) {
     if (Newspaper[i] == 1) {
@@ -52,7 +55,7 @@ double calcNewspaper(int Newspaper[6]) {
         return 0.0;
     }
 }
-void billCost(char bookingID[], char mainFirstGuestName[6],mainSecondGuestName[6], double roomTotal[6], double boardTotal[6], double newspaperCost[6]) {
+void billCost(bookingID[], mainFirstGuestName[6],mainSecondGuestName[6], roomTotal[6],boardTotal[6],newspaperCost[6]) {
     double overallCost = roomTotal[i] + boardTotal[i] + newspaperCost[i];
     printf("Total cost");
     printf("Booking ID: %s\n", bookingID[i]);
