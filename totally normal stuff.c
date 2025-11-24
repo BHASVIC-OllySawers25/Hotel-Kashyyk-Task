6b1db5fcfeed53dddf683eaa0dcd73deb8e16d1f
Olly <olly.sawers25@students.bhasvic.ac.uk>
2025-11-24 09:39:06 +0000

    Update totally normal stuff.c

--- totally normal stuff.c
+++ totally normal stuff.c
@@ -1,38 +1,69 @@
-// C++ program to display hostname
+// C program to display hostname
 // and IP address
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <errno.h>
 #include <netdb.h>
- 
-// Function use 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// Returns hostname for the local computer
+void checkHostName(int hostname)
+{
+    if (hostname == -1)
+    {
+        perror("gethostname");
+        exit(1);
+    }
+}
+
+// Returns host information corresponding to host name
+void checkHostEntry(struct hostent * hostentry)
+{
+    if (hostentry == NULL)
+    {
+        perror("gethostbyname");
+        exit(1);
+    }
+}
+
+// Converts space-delimited IPv4 addresses
+// to dotted-decimal format
+void checkIPbuffer(char *IPbuffer)
+{
+    if (NULL == IPbuffer)
+    {
+        perror("inet_ntoa");
+        exit(1);
+    }
+}
+
+// Driver code
 int main()
 {
     char hostbuffer[256];
+    char *IPbuffer;
     struct hostent *host_entry;
     int hostname;
-    struct in_addr **addr_list;
 
-    // retrieve hostname
+    // To retrieve hostname
     hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-    if (hostname == -1) {
-        perror("gethostname error");
-        exit(1);
-    }
-    printf("Hostname: %s\n", hostbuffer);
+    checkHostName(hostname);
 
-    // Retrieve IP addresses
+    // To retrieve host information
     host_entry = gethostbyname(hostbuffer);
-    if (host_entry == NULL) {
-        perror("gethostbyname error");
-        exit(1);
-    }
-    addr_list = (struct in_addr **)host_entry->h_addr_list;
-    for (int i = 0; addr_list[i] != NULL; i++) {
-        printf("IP address %d: %s\n", i+1, inet_ntoa(*addr_list[i]));
-    }
+    checkHostEntry(host_entry);
+
+    // To convert an Internet network
+    // address into ASCII string
+    IPbuffer = inet_ntoa(*((struct in_addr*)
+                        host_entry->h_addr_list[0]));
+
+    printf("Hostname: %s\n", hostbuffer);
+    printf("Host IP: %s", IPbuffer);
 
     return 0;
 }
